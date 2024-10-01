fde027bf4a2fe6461ff2d8efa4c0e8fa1e600eb6
Terrin <terrinsusirani@gmail.com>
2024-10-01 13:27:06 +0530

    Nav Person

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import day_logo from '../../assets/day.png';
-//import light_logo from '../../assets/night.png';
-//import dark_person from '../../assets/person_dark.png';
 import light_person from '../../assets/person_light.png';
 
- const Navbar = () => {
-  // const toggle_mode = () => {
-  //   setTheme(theme === 'light' ? 'dark' : 'light');
-  //   console.log(theme);  // Logs the old theme before the state updates
-  // };
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const Menus = ['Profile', 'Help', 'Settings', 'Logout'];
 
   return (
-    <div className={`container`}>
-      <img src={light_person } alt="" className='toggle-icon' />
-      <img 
-        // onClick={toggle_mode} // Change from onCanPlay to onClick
-        src={ day_logo} 
-        alt="" 
-        className='toggle-icon' 
-      />
+    <div className="container">
+      <img onClick={() => setOpen(!open)} src={light_person} alt="" className="toggle-icon" />
+      <img src={day_logo} alt="" className="toggle-icon" />
+
+      {open && (
+        <div className="dropdown">
+          <ul>
+            {Menus.map((menu, index) => (
+              <li onClick={() => setOpen(!open)} className="list" key={index}>
+                {menu}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
